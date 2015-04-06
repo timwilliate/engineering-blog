af2c7da3990d1131f4f551191b73ba48ea104778
philcryer <phil@philcryer.com>
2015-04-06 14:29:53 -0500

    simplier deploy

--- update_prod.sh
+++ update_prod.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -d '_site' ]; then
+    echo "ERROR cannot find dist directory _site, run jekyll build to create it"
+    exit 1
+git push origin `git subtree split --prefix _site master`:gh-pages --force
+
+exit 0
+
+
+another possible method
+
 git checkout gh-pages
 git rebase master
 git push
