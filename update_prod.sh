0a3a7ba726c56985cba41b10b8923465e5c26355
philcryer <phil@philcryer.com>
2015-04-06 14:43:14 -0500

    new update method, this could work

--- update_prod.sh
+++ update_prod.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-if [ ! -d '_site' ]; then
-    echo "ERROR cannot find dist directory _site, run jekyll build to create it"
-    exit 1
-fi
-git push origin `git subtree split --prefix _site master`:gh-pages --force
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
-exit 0
+SELF=`basename $0`
+SOURCE_BRANCH="master"
+DEST_BRANCH="gh-pages"
+TMP_LOG=`git log | head -n1 | cut -d" " -f2`
+TMP_DIR="tmp-$TMP_LOG"
 
-
-another possible method
-
-git checkout gh-pages
-git rebase master
-git push
-git checkout master
+git checkout $SOURCE_BRANCH
+jekyll build -d $TMP_DIR
+git checkout $DEST_BRANCH
+git rm -qr .
+cp -r $TMP_DIR/. .
+rm ./$SELF
+rm -r $TMP_DIR
+git add -A
+git commit -m "Published updates on `date`"
+git push origin $DEST_BRANCH
+git checkout $SOURCE_BRANCH
 
 exit 0
