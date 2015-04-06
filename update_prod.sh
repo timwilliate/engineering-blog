ddefb870bd652353d7dc7bee39d22f48cc9c27ea
philcryer <phil@philcryer.com>
2015-04-06 14:21:25 -0500

    use rebase to sync gh-pages with master - only on demand - not a hook

--- update_prod.sh
+++ update_prod.sh
@@ -1,52 +1,8 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-
-#URL=$1
-URL="https://github.com/MonsantoCo/engineering-blog"
-SRC=$(pwd)
-TEMP=$(mktemp -d -t eng-XXX)
-trap "rm -rf ${TEMP}" EXIT
-CLONE=${TEMP}/clone
-
-echo -e "Cloning Github repository:"
-git clone "${URL}" "${CLONE}"
-cd "${CLONE}"
-
-echo -e "\nBuilding Jekyll site:"
-rm -rf _site
-
-if [ -r _config-deploy.yml ]; then
-  jekyll build --config _config.yml,_config-deploy.yml
-else
-  jekyll build
-fi
-
-if [ ! -e _site ]; then
-  echo -e "\nJekyll didn't generate anything in _site!"
-  exit -1
-fi
-
-cp -R _site ${TEMP}
-
-echo -e "\nPreparing gh-pages branch:"
-if [ -z "$(git branch -a | grep origin/gh-pages)" ]; then
-  git checkout --orphan gh-pages
-else
-  git checkout gh-pages
-fi
-
-echo -e "\nDeploying into gh-pages branch:"
-rm -rf *
-cp -R ${TEMP}/_site/* .
-rm -f README.md
-git add .
-git commit -am "new version $(date)" --allow-empty
-git push origin gh-pages 2>&1 | sed 's|'$URL'|[skipped]|g'
-
-echo -e "\nCleaning up:"
-rm -rf "${CLONE}"
-rm -rf "${SITE}"
+git checkout gh-pages
+git rebase master
+git push
+git checkout master
 
 exit 0
